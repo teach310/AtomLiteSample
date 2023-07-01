7af2cb4b02b655cf79fa463c7c0646db777d9c2d
teach310 <okms310@yahoo.co.jp>
2023-07-01 22:27:39 +0900

    不要なコメント等を削除

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <M5Unified.h>
 #include <FastLED.h>
 
-// RGB LEDの数を指定(M5Atom Matrixなら25)
 #define NUM_LEDS 1
-// RGB LEDのDATA PINを指定
 #define LED_DATA_PIN 27
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
@@ -18,13 +16,11 @@ bool isAvailable()
 
 void setup()
 {
-
   auto cfg = M5.config();
   M5.begin(cfg);
   Wire.begin(25, 21);
   FastLED.addLeds<WS2811, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(20);
-  Serial.print("Hello!!");
   if (isAvailable())
   {
     Serial.println("VL53L0X found");
@@ -39,7 +35,7 @@ void setup()
     while (1)
       ;
   }
-  Serial.println(F("VL53L0X API Continuous Ranging example\n\n"));
+  Serial.println(F("Start App\n\n"));
   lox.startRangeContinuous();
 }
 
